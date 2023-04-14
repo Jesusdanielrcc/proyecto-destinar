391d844d86af4c8b015c63e8356d2b1ff581c3d6
jesusdanielrcc <jesusdanielrccc@gmail.com>
2023-04-14 04:31:06 -0300

    modificaciones

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
-    $('nav.nav1 a:first').addClass('active');
-    $('').hide();
-    $('').show(); 
+    $('ul.tabs li a:first').addClass('active');
+    $('.parrafito, .formulario1, .formulario2').hide();
+    $('.slider').show(); 
 
-    $('nav.nav1 a').click(function(){
-        $('nav.nav1 a').removeClass('active');
+$('ul.tabs li a').click (function(){
+	$('ul.tabs li a').removeClass('active');
         $(this).addClass('active');
-        $('').hide();
+        $('.slider').hide();
 
         var activeTab = $(this).attr('href');
+	console.log(activeTab)
         $(activeTab).show();
         return false;
     });
