e581f13e1faf3b6e243d4af1f98f613824c03274
Jesús Rodríguez <jesusdanielrccc@gmail.com>
2023-04-17 10:17:06 -0300

    modificacion3

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ $('ul.tabs li a').click (function(){
         $(this).addClass('active');
         $('.slider').hide();
 
-        var activeTab = $(this).attr('href');
+    var activeTab = $(this).attr('href');
 	console.log(activeTab)
-        $(activeTab).show();
-        return false;
+    $(activeTab).show();
+    return false;
     });
 });
 
@@ -22,6 +22,30 @@ function captura(){
     var plan=document.getElementById("info4").value;
     var email=document.getElementById("info5").value;
     console.log(personas +" "+ fechas +" "+ destino +" "+ plan +" "+ email)
+    if (personas==""){
+        alert("Todos los campos son obligatorios");
+        document.getElementById("info1").focus();
+    }
+
+    if (fechas==""){
+        alert("Todos los campos son obligatorios");
+        document.getElementById("info2").focus();
+    }
+
+    if (destino==""){
+        alert("Todos los campos son obligatorios");
+        document.getElementById("info3").focus();
+    }
+
+    if (plan==""){
+        alert("Todos los campos son obligatorios");
+        document.getElementById("info4").focus();
+    }
+
+    if (email==""){
+        alert("Todos los campos son obligatorios");
+        document.getElementById("info5").focus();
+    }
 }
 
 function captura2(){
@@ -30,4 +54,23 @@ function captura2(){
     var numero=document.getElementById("info8").value;
     var mail=document.getElementById("info9").value;
     console.log(nombre +" "+ apellido +" "+ numero +" "+ mail)
+    if (nombre==""){
+        alert("Todos los campos son obligatorios");
+        document.getElementById("info6").focus();
+    }
+
+    if (apellido==""){
+        alert("Todos los campos son obligatorios");
+        document.getElementById("info7").focus();
+    }
+
+    if (numero==""){
+        alert("Todos los campos son obligatorios");
+        document.getElementById("info8").focus();
+    }
+
+    if (mail==""){
+        alert("Todos los campos son obligatorios");
+        document.getElementById("info9").focus();
+    }
 }
